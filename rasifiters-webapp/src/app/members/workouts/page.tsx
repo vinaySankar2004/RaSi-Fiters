1014acab74bbad46b66438584623c6fcd83e9313
Vinayak Sankaranarayanan <vinayaksankaranarayanan@dhcp-206-87-247-154.ubcsecure.wireless.ubc.ca>
2026-02-07 18:13:14 -0800

    minor deployment fixes

--- rasifiters-webapp/src/app/members/workouts/page.tsx
+++ rasifiters-webapp/src/app/members/workouts/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useMemo, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
